69a2ccbc2257cd9bf7b85aeee1b6366229b42f05
wchung34 <wchung34@my.centennialcollege.ca>
2022-09-24 12:00:42 -0400

    Simple Express example

--- server.js
+++ server.js
@@ -1,40 +1,11 @@
-// const hello = require("./hello");
+const express = require('express')
+const app = express()
+const port = 3000
 
-// hello.sayHello();
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-// const lib = require("./lib");
-// console.log(lib.halfOf(10));
-
-// // Node
-// const http = require("http");
-// http.createServer((request, response) => {
-//     response.writeHead(200, {"Content-Type": "text/plain"});
-//     response.end("Hello, World!");
-// }).listen(3000);
-
-// console.log("Server running at http://localhost:3000/");
-
-const connect = require('connect');
-const app = connect();
-app.listen(3000);
-
-const logger = (request, response, next) => {
-    console.log(request.method, request.url);
-    next();
-};
-
-const helloWorld = (request, response, next) => {
-    response.setHeader('Content-Type', 'text/plain');
-    response.end('Hello, World!');
-};
-
-const goodbye = (request, response, next) => {
-    response.setHeader('Content-Type', 'text/plain');
-    response.end('Good bye!');
-};
-
-app.use(logger);
-app.use('/hello', helloWorld);
-app.use('/goodbye', goodbye);
-
-console.log("Server running at http://localhost:3000/");
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
